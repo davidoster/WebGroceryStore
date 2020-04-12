e5984683285e4a2426a59c89405e9141d4e84580
David Oster <paspa@hotmail.com>
2020-04-12 23:58:40 +0300

    servlet

--- src/java/controllers/Products.java
+++ src/java/controllers/Products.java
@@ -46,7 +46,7 @@ public class Products extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Servlet Products at " + request.getContextPath() + "</h1>");
-            Class.forName("com.mysql.cj.jdbc");
+            Class.forName("com.mysql.cj.jdbc.Driver");
             Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/grocerystore?useSSL=false&serverTimezone=UTC",
                                           "root", "Root1234!");
             Statement s = c.createStatement();
